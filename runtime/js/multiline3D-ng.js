a4e3b85efb4f4ebcbfc3e8a06e916691b4f153db
Unfaehig <simon.bruegel@gmx.de>
2020-08-03 11:42:50 +0200

    Fix init issue

--- runtime/js/multiline3D-ng.js
+++ runtime/js/multiline3D-ng.js
@@ -5,8 +5,193 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
 (function () {
   'use strict';
 
+  var VF_ANG_Ext = {};
+
+  VF_ANG_Ext.getMultilineLabelHeightAttribute = function getMultilineLabelHeightAttribute(element) {
+    //var height = element.attr('height');
+    let padding = element.attr('padding');
+    let font = element.attr('font');
+    let linecount = element.attr('linecount');
+    let width = element.attr('width');
+    var height = (parseInt(padding.match(/[\^\d+]/g,).join(""))*2+1.2*parseInt(font.match(/[\^\d+]/g).join(""))*linecount)/(1024/parseFloat(width));
+    //height = (parseInt(element.attr('padding').replace(/[^\d]/g, '')) * 2 + 1.2 * parseInt(element.attr('font').replace(/[^\d]/g, '')) * element.attr('linecount')) / (1024 / parseFloat(element.attr('width')));
+    console.log(height,width,padding,font,linecount)
+    return height;
+  }
+
+  VF_ANG_Ext.multilinehelper = function multilinehelper(context, text, x, y, lineHeight, fitWidth, fitHeight) {
+    fitWidth = fitWidth || 0;
+    if (fitWidth <= 0) {
+      context.fillText(text, x, y);
+      return;
+    }
+    var words = text.split(' ');
+    for (var k = 0; k < words.length; k++) {
+      if (/^(\r|\n)/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
+        console.log("Starts with line break");
+        words.splice(k + 1, 0, words[k].replace(/^(\r|\n)/, ''));
+        words[k] = '\n'
+        k++;
+      }
+      if (/(\r|\n)$/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
+        console.log("Ends with line break");
+        words[k] = words[k].replace(/(\r|\n)$/, '');
+        k++;
+        words.splice(k, 0, '\n');
+      }
+    }
+    var currentLine = 0;
+    var idx = 1;
+    while (words.length > 0 && idx <= words.length) {
+      if (y + (lineHeight * (currentLine+1)) >= fitHeight) {
+        console.warn(" The text is to long to store in this widget. Change font size or use shorter text!");
+        break;
+      }
+      if (words[idx - 1] !== '\n') {
+        var str = words.slice(0, idx).join(' ');
+        var w = context.measureText(str).width;
+        if (w > fitWidth) {
+          if (idx == 1)
+            idx = 2;
+          context.fillText(words.slice(0, idx - 1).join(' ').trim(), x, y + (lineHeight * currentLine));
+          currentLine++;
+          words = words.splice(idx - 1);
+          idx = 1;
+        } else
+          idx++;
+      } else {
+        if (idx == 1)
+          idx = 2;
+        context.fillText(words.slice(0, idx - 1).join(' '), x, y + (lineHeight * currentLine));
+        currentLine++;
+        words = words.splice(idx);
+        idx = 1;
+      }
+    }
+    if (idx > 0)
+      context.fillText(words.join(' '), x, y + (lineHeight * currentLine));
+  }
+
+  // Preview can only use HEX in cavas so we convert here to get same results
+  VF_ANG_Ext.rgb2hex = function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgb.length === 4) ? "#" +
+      ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+  }
+
+  //////////////////////////////////////////////////////////////////////
+  // Draws an Background Image and a label on a 3D Image
+  //
+  VF_ANG_Ext.bulidMultilineLabel = function bulidMultilineLabel(props) {
+
+    // Add new property to check if the function is only called once. If we don't do it we loop the image!
+    //var wdg = attr.id;
+
+    let canvas = document.createElement("canvas");
+    let fontSize = parseInt(props.font.match(/[\^\d+]/g,).join(""));
+    let padding = props.padding ? parseInt(props.padding.match(/[\^\d+]/g,).join("")) : 20;
+    let lineCount = props.linecount ? parseInt(props.linecount) : 5;
+    canvas.width = 1024;
+    canvas.height = padding * 2 + fontSize * 1.2 * lineCount;
+    console.log(fontSize,padding,lineCount,canvas.height);
+    let ctx = canvas.getContext("2d");
+
+
+    ctx.fillStyle = VF_ANG_Ext.rgb2hex(props.fontcolor); //pass in font color prop
+
+    ctx.font = props.font ? props.font : "bold 70px Arial"; // Segoe only works with some tricky stuff see Styles for more information
+    // we may need to adjust y to take multi-line into account e..g if there are 2 lines, we need to move y(start) up 
+    VF_ANG_Ext.multilinehelper(ctx, props.text, padding, fontSize, fontSize * 1.2, canvas.width - (2 * padding),canvas.height-padding);
+    var newimg = canvas.toDataURL()// + '#edge=clamp';
+
+    //wdg.src = newimg; // need to save it back to src property becuase the texture will updated with the last set Image if you change for example colors
+    //scope.textureField = newimg; // on hololens we need this aswell becuase he will not change it on first click!
+
+    //scope.src = newimg;
+    //renderer.setTexture(wdg, newimg);
+    return newimg;
+
+  }
+
   var Multiline3DModule = angular.module('multiline3D-ng', []);
-  Multiline3DModule.directive('ngMultiline3d', ['$timeout', 'tml3dRenderer', ngMultiline3d]);
+  Multiline3DModule.directive('twxDtMultilinelabel', ['tml3dRenderer', '$animate', function (renderer, $animate) {
+    function getLabelAttributes(el) {
+      return {
+        text: el.attr('text'),
+        padding: el.attr('padding'),
+        font: el.attr('font'),
+        fontcolor: el.attr('fontcolor'),
+        linecount: el.attr('linecount'),
+        height: VF_ANG_Ext.getMultilineLabelHeightAttribute(el),
+        width: el.attr('width')
+      };
+    }
+
+    return {
+      restrict: 'E',
+      require: '^twxDtTracker',
+      link: function (scope, element, attrs, ctrl) {
+        // We have to backup tracker as it will change when next twxDtTracker is encountered by Angular.
+        var tracker = scope.tracker;
+
+        var addFunction = function addFunction(addFunctionAddAPICallCallback, addFunctionSuccessCallback,
+          addFunctionFailureCallback) {
+          var asset = VF_ANG.createObj("twx-dt-label", element);
+          $animate.enabled(element, false);
+
+          asset.addAsset = function () {
+            var successCallback = function () {
+              addFunctionSuccessCallback();
+
+              VF_ANG.addAssetCallback(scope, asset, renderer);
+            };
+
+            var failureCallback = function (error) {
+              addFunctionFailureCallback();
+
+              console.log("twx-dt-label: Error in add3DImage id: [" + element.attr("id") + "] due to [" + error + "]");
+            };
+
+            VF_ANG.addAsset(renderer, asset, function () {
+              addFunctionAddAPICallCallback();
+
+              var properties = getLabelAttributes(element);
+              renderer.add3DImage(
+                tracker.name,
+                element.attr("id"),
+                VF_ANG_Ext.bulidMultilineLabel(properties),
+                undefined,
+                undefined,
+                undefined,
+                undefined,
+                element.attr("width"),
+                properties.height, //Calculated height from textToImage includes border,padding,etc.
+                element.attr("pivot"),
+                successCallback,
+                failureCallback
+              );
+            });
+          };
+
+          ctrl.addAssetOrQueueForAdditionIfNeeded(tracker, asset);
+        };
+
+        var setFunction = function setFunction() {
+          renderer.setTexture(element.attr('id'), VF_ANG_Ext.bulidMultilineLabel(getLabelAttributes(element)));
+        };
+
+        var attributeNames = ['padding', 'width', 'text', 'font','fontcolor'];
+
+        var addSetTriggerFunctions = VF_ANG.setupWatchWithAddSetConvention(scope, element, attributeNames, addFunction, setFunction);
+
+        VF_ANG.addStyleReadyListener(element, addSetTriggerFunctions.eventFunction);
+      }
+    };
+  }]);
+  /*Multiline3DModule.directive('ngMultiline3d', ['$timeout', 'tml3dRenderer', ngMultiline3d]);
+
   function ngMultiline3d($timeout, renderer) {
 
     return {
@@ -17,71 +202,72 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
         width: '@',
         font: '@',
         fontcolor: '@',
+        linecount: '@',
         padding: '@',
         src: '@'
         //id: '='
       },
       link: function (scope, element, attr) {
+        //scope.calcHeight = function() {
+        attr.height = (scope.padding.replace(/[\^\d]/g, '') * 2 + 1.2 * parseInt(scope.font.replace(/[\^\d]/g, '')) * scope.linecount) / (1024 / parseFloat(scope.width));
+        console.log(attr.height)
+        //}
+
         scope.canvasheight;
-        
-        var multilinehelper = function ( context , text, x, y, lineHeight, fitWidth) {
+
+        var multilinehelper = function (context, text, x, y, lineHeight, fitWidth) {
           fitWidth = fitWidth || 0;
-          if (fitWidth <= 0)
-          {
-            context.fillText( text, x, y );
+          if (fitWidth <= 0) {
+            context.fillText(text, x, y);
             return;
           }
           var words = text.split(' ');
-          for(var k = 0; k<words.length; k++) {
-            if(/^(\r|\n)/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
+          for (var k = 0; k < words.length; k++) {
+            if (/^(\r|\n)/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
               console.log("Starts with line break");
-              words.splice(k+1, 0, words[k].replace(/^(\r|\n)/,''));
+              words.splice(k + 1, 0, words[k].replace(/^(\r|\n)/, ''));
               words[k] = '\n'
               k++;
             }
-            if(/(\r|\n)$/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
+            if (/(\r|\n)$/.exec(words[k]) && !/^(\r|\n)$/g.exec(words[k])) {
               console.log("Ends with line break");
-              words[k] = words[k].replace(/(\r|\n)$/,'');
+              words[k] = words[k].replace(/(\r|\n)$/, '');
               k++;
               words.splice(k, 0, '\n');
             }
           }
           var currentLine = 0;
           var idx = 1;
-          while (words.length > 0 && idx <= words.length)
-          {
-            if(y + (lineHeight*currentLine) > scope.canvasheight)
+          while (words.length > 0 && idx <= words.length) {
+            if (y + (lineHeight * currentLine) > scope.canvasheight)
               console.warn(attr.id + " The text is to long to store in this widget. Change font size or use shorter text!");
-            if(words[idx-1] !== '\n') {
-              var str = words.slice(0,idx).join(' ');
+            if (words[idx - 1] !== '\n') {
+              var str = words.slice(0, idx).join(' ');
               var w = context.measureText(str).width;
-              if ( w > fitWidth )
-              {
-                if (idx==1)
-                  idx=2;
-                context.fillText( words.slice(0,idx-1).join(' ').trim(), x, y + (lineHeight*currentLine) );
+              if (w > fitWidth) {
+                if (idx == 1)
+                  idx = 2;
+                context.fillText(words.slice(0, idx - 1).join(' ').trim(), x, y + (lineHeight * currentLine));
                 currentLine++;
-                words = words.splice(idx-1);
+                words = words.splice(idx - 1);
                 idx = 1;
-              }
-              else
+              } else
                 idx++;
-            }
-            else {
-              if (idx==1)
-                idx=2;
-              context.fillText( words.slice(0,idx-1).join(' '), x, y + (lineHeight*currentLine) );
+            } else {
+              if (idx == 1)
+                idx = 2;
+              context.fillText(words.slice(0, idx - 1).join(' '), x, y + (lineHeight * currentLine));
               currentLine++;
               words = words.splice(idx);
               idx = 1;
-            }  
+            }
           }
           if (idx > 0)
-            context.fillText( words.join(' '), x, y + (lineHeight*currentLine) );
+            context.fillText(words.join(' '), x, y + (lineHeight * currentLine));
         }
 
         // Preview can only use HEX in cavas so we convert here to get same results
-        var rgb2hex = function(rgb) {
+        var rgb2hex = function (rgb) {
           rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
           return (rgb && rgb.length === 4) ? "#" +
             ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
@@ -92,47 +278,52 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
         //////////////////////////////////////////////////////////////////////
         // Draws an Background Image and a label on a 3D Image
         //
-        var bulidMultilineLabel = function () {         
+        var bulidMultilineLabel = function () {
 
           // Add new property to check if the function is only called once. If we don't do it we loop the image!
           var wdg = attr.id;
 
           let canvas = document.createElement("canvas");
-          canvas.width = 1024;
-          canvas.height = scope.canvasheight = 1024;
-          let ctx = canvas.getContext("2d");
+          let fontSize = parseInt(scope.font.substr(scope.font.indexOf(" ", 1), scope.font.indexOf("px", 1) - 1));
           let padding = scope.padding ? parseInt(scope.padding) : 20;
+          let lineCount = scope.linecount ? parseInt(scope.linecount) : 5;
+          canvas.width = 1024;
+          canvas.height = scope.canvasheight = padding * 2 + fontSize * 1.2 * lineCount;
+          console.log(canvas.height);
+          let ctx = canvas.getContext("2d");
+
 
           ctx.fillStyle = rgb2hex(scope.fontcolor); //pass in font color prop
-  
+
           ctx.font = scope.font ? scope.font : "bold 70px Arial"; // Segoe only works with some tricky stuff see Styles for more information
-          let fontSize = parseInt(scope.font.substr(scope.font.indexOf(" ", 1), scope.font.indexOf("px", 1) - 1))
           // we may need to adjust y to take multi-line into account e..g if there are 2 lines, we need to move y(start) up 
-          multilinehelper(ctx, scope.text, padding, fontSize, fontSize*1.2, canvas.width-(2*padding));
+          multilinehelper(ctx, scope.text, padding, fontSize, fontSize * 1.2, canvas.width - (2 * padding));
           var newimg = canvas.toDataURL() + '#edge=clamp';
 
           //wdg.src = newimg; // need to save it back to src property becuase the texture will updated with the last set Image if you change for example colors
           //scope.textureField = newimg; // on hololens we need this aswell becuase he will not change it on first click!
-          
+
           scope.src = newimg;
           renderer.setTexture(wdg, newimg);
-        
-          
+
+
         }
         //////////////////////////////////////////////////////////////////////
         // monitor state
         //
-        scope.$watchGroup(['text', 'font', 'fontColor','padding'], function () {
+        scope.$watchGroup(['text', 'font', 'fontColor', 'padding'], function () {
           bulidMultilineLabel();
         });
 
         // make sure we are triggered when the page is ready    
         scope.$root.$on("$ionicView.afterEnter", function (event) {
-          $timeout(function(){bulidMultilineLabel();},1500);
+          $timeout(function () {
+            bulidMultilineLabel();
+          }, 1500);
         });
 
       }
     };
-  }
+  }*/
 
 }());
--- widgets/rdlabsMultilineLabel/multilineLabel3D.design.js
+++ widgets/rdlabsMultilineLabel/multilineLabel3D.design.js
@@ -24,6 +24,12 @@ function twxMultiLineLabel3D() {
       label: 'ves-ar-extension:Font',
       datatype: 'string',
       default: 'bold 70px Arial',
+      editor: 'select',
+      options: [
+        {label: 'Small', value: "bold 70px Arial"},
+        {label: 'Medium', value: "bold 90px Arial"},
+        {label: 'Large', value: "bold 120px Arial"}
+      ],
       isBindingTarget: true,
       sortOrder: 3
     },
@@ -36,33 +42,40 @@ function twxMultiLineLabel3D() {
       sortOrder: 4
     },
     {
+      name: 'lineCount',
+      label: 'Max Lines',
+      datatype: 'number',
+      tFrag: "linecount={{me.lineCount}} height={{ctrl.delegate.calcHeight()}}",//"height={{calcHeight()}}",//"height={{(parseInt(me.padding.replace(/[\^\d]/g,''))*2)*(1.2*parseInt(me.font.replace(/[\^\d]/g,''))*me.lineCount)/(1024/me.width)}}",
+      default: 5,
+      isBindingTarget: true,
+      sortOrder: 5
+    },
+    {
       name: 'padding',
       label: 'ves-ar-extension:Padding',
       datatype: 'string',
-      default: '20',
+      default: '20px',
       isBindingTarget: true,
-      sortOrder: 5
+      sortOrder: 6
     }
   ];
 
-  var setdefault = Twx3dCommon.getWidthProperty();
-  setdefault.default = "0.1";
-  properties.push(setdefault);
-  setdefault = Twx3dCommon.getHeightProperty();
-  //setdefault.default = "0.1";
-  properties.push(setdefault);
   properties.push(Twx3dCommon.getPivotProperty());
 
   var overlay = Twx3dCommon.arrayToMap(properties);
   overlay.experimentalOneSided = Twx3dCommon.getOneSidedProperty();
   overlay.experimentalOneSided.default = true;
-  //overlay.placeholder_img = Twx3dCommon.getPlaceHolderImgProperty('/extensions/images/Gate.png');
+  overlay.width = Twx3dCommon.getWidthProperty();
+  overlay.width.default = 0.1;
+  overlay.placeholder_img = Twx3dCommon.getPlaceHolderImgProperty('/extensions/images/Gate.png');
 
-  var props = Twx3dCommon.new3dProps(overlay);
+  var props = Twx3dCommon.new3dProps(overlay, ['scale']);
 
-  var runtimeTemplate = Twx3dCommon.buildRuntimeTemplate('twx-dt-image', props);
+
+  var runtimeTemplate = Twx3dCommon.buildRuntimeTemplate('twx-dt-multilinelabel', props);
+  var designTemplate = Twx3dCommon.buildRuntimeTemplate('twx-dt-image',Twx3dCommon.new3dProps(overlay, ['scale','text']));
   return {
-    elementTag: 'twx-multilinelabel3D',
+    elementTag: 'twx-dt-multilinelabel',
 
     label: '3D Multiline Label',
 
@@ -84,12 +97,30 @@ function twxMultiLineLabel3D() {
       angularModules: ['multiline3D-ng']
     },
 
+    delegate: function () {
+      let ctrl;
+
+      this.init = function (element, widgetCtrl) {
+        ctrl = widgetCtrl;
+      };
+
+      /**
+       * @returns {Array} Returns the paths for each of the resources this widget references.  Will be used to remove
+       *         the resources when the widget is removed.
+       */
+      this.calcHeight = function() {
+        let props = ctrl.properties;
+        return (parseInt(props.padding.value.match(/[\^\d+]/g,).join(""))*2+1.2*parseInt(props.font.value.match(/[\^\d+]/g).join(""))*props.lineCount.value)/(1024/parseFloat(props.width.value));
+      };
+
+    },
+
     designTemplate: function () {
-      return runtimeTemplate;
+      return designTemplate//.replace('width="{{me.width}}"', 'height="{{(me.padding*2)*(1.2*me.font.replace( /[^\d]/g,""))*me.lineCount)/(1024/me.width)}}" width="{{me.width}}"');
     },
 
     runtimeTemplate: function (props) {
-      var template3d = runtimeTemplate.replace("#widgetId#", props.widgetId).replace('>', ' ng-multiline3d>').replace('src="{{me.src}}"', 'src="extensions/images/navfeet.png"');
+      var template3d = runtimeTemplate.replace("#widgetId#", props.widgetId).replace('src="{{me.src}}"', 'src="extensions/images/navfeet.png" sx="1" sy="1" sz="1"');
       return template3d;
     }
   };
